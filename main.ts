b3275a04586bb46c0dacfb57d2543eba97f87740
LDPCICNM <78750096+LDPCICNM@users.noreply.github.com>
2021-04-19 18:21:32 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,39 @@
+let Distancia = 0
+let httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro = 0
+irRemote.connectInfrared(DigitalPin.P16)
+basic.forever(function () {
+    httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro = irRemote.returnIrButton()
+    Distancia = k_Bit.ultra()
+})
+basic.forever(function () {
+    if (httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro == 70) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            `)
+        k_Bit.run(DIR.RunForward, 67)
+    } else if (httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro == 64) {
+        k_Bit.carStop()
+    } else if (httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro == 64) {
+    	
+    } else if (httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro == 67) {
+        k_Bit.run(DIR.TurnRight, 50)
+    } else if (httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro == 68) {
+        k_Bit.run(DIR.TurnLeft, 50)
+    } else if (httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro == 21) {
+        k_Bit.run(DIR.RunBack, 67)
+    } else if (httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro == 25) {
+        if (httpsldpcicnmgithubiomenudocachodeprolanderitoguayqueesunpro > 15) {
+            while (true) {
+            	
+            }
+        } else {
+        	
+        }
+    } else {
+    	
+    }
+})
