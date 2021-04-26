3f09665c4c9eed03e82505b4fac8c5e7d1b67489
LDPCICNM <78750096+LDPCICNM@users.noreply.github.com>
2021-04-26 18:14:44 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,43 +7,15 @@ basic.forever(function () {
         k_Bit.carStop()
     } else {
         if (Guay == 70) {
-            basic.showLeds(`
-                . . . . .
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                `)
             k_Bit.run(DIR.RunForward, 67)
         } else if (Guay == 64) {
             k_Bit.carStop()
         } else if (Guay == 67) {
-            basic.showLeds(`
-                . . . # .
-                . . # . .
-                . # # # #
-                . . # . .
-                . . . # .
-                `)
             k_Bit.run(DIR.TurnRight, 50)
         } else if (Guay == 68) {
-            basic.showLeds(`
-                . # . . .
-                . . # . .
-                # # # # .
-                . . # . .
-                . # . . .
-                `)
             k_Bit.run(DIR.TurnLeft, 50)
         } else if (Guay == 21) {
             k_Bit.run(DIR.RunBack, 67)
-            basic.showLeds(`
-                . . # . .
-                # . # . #
-                . # # # .
-                . . # . .
-                . . . . .
-                `)
         }
     }
 })
